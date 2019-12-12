14823cae23a4bd32a30d186e888bc7648b242e66
CFH <57480676+cosmikflaghunters@users.noreply.github.com>
2019-12-12 12:40:30 +0100

    Update soft.sh

--- soft.sh
+++ soft.sh
@@ -5,3 +5,7 @@ git clone https://github.com/Ganapati/RsaCtfTool.git
 git clone https://github.com/DominicBreuker/stego-toolkit.git
 git clone https://github.com/maurosoria/dirsearch.git
 git clone https://github.com/ticarpi/jwt_tool.git
+pip3 install stegoveritas
+stegoveritas_install_deps
+apt-get install steghide -y
+pip3 install pycryptodomex
