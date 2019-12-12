c7ca077f6c4ee5b51ca44e8c853dbec1c98daa29
CFH <57480676+cosmikflaghunters@users.noreply.github.com>
2019-12-12 12:03:58 +0100

    Create soft.sh

---
+++ soft.sh
@@ -0,0 +1,7 @@
+git clone https://github.com/bannsec/stegoVeritas.git
+git clone https://github.com/StefanoDeVuono/steghide.git
+git clone https://github.com/Paradoxis/StegCracker.git
+git clone https://github.com/Ganapati/RsaCtfTool.git
+git clone https://github.com/DominicBreuker/stego-toolkit.git
+git clone https://github.com/maurosoria/dirsearch.git
+git clone https://github.com/ticarpi/jwt_tool.git
